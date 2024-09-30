d32624bec0ce4726d3d0973c7477b614bde5ce5a
EverViera12 <evertigre10@gmail.com>
2024-09-30 11:01:40 -0600

    delete method

--- app/productos/page.js
+++ app/productos/page.js
@@ -3,15 +3,43 @@
 import { Grid } from "gridjs-react";
 import { _ } from "gridjs-react";
 import "gridjs/dist/theme/mermaid.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFile, faFileEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { handleSubmit } from "@/fetching/fetchProductos";
 import { useState } from "react";
 import ProductsForm from "@/components/Forms/ProductsForm";
 import ActionsTable from "@/components/ActionsTable";
+import Loader from "@/components/Loader";
 
 export default function ProductosPage() {
   const [modal, setModal] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const handleDelete = async () => {
+    if (!selectedProduct) return;
+
+    const confirmDelete = window.confirm(
+      `¿Estás seguro de que quieres eliminar el producto ${selectedProduct.nombre}?`
+    );
+    if (!confirmDelete) return;
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/productos/producto/${selectedProduct.id}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (response.ok) {
+        alert("Producto eliminado con éxito");
+        window.location.reload();
+      } else {
+        const errorData = await response.json();
+        alert(`Error al eliminar el producto: ${errorData.message}`);
+      }
+    } catch (error) {
+      alert("Error al eliminar el producto: " + error.message);
+    }
+  };
 
   return (
     <div className="grid px-10 gap-5 py-16">
@@ -21,31 +49,46 @@ export default function ProductosPage() {
         <ProductsForm handleSubmit={handleSubmit} setModal={setModal} />
       )}
 
-      <Grid
-        server={{
-          url: "http://localhost:3000/productos",
-          handle: async (res) => {
-            if (res.status === 404) return await { data: [] };
-            if (res.ok) return await res.json();
+      {loading && <Loader />}
 
-            throw new Error("oh no :(");
-          },
-          then: (data) =>
-            data.map((producto) => [
-              producto.nombre,
-              producto.descripcion,
-              producto.precio,
-              producto.stock,
-              _(<ActionsTable setModal={setModal} />),
-            ]),
-        }}
-        columns={["Nombre", "Descripción", "Precio", "Stock", "Acciones"]}
-        search={true}
-        sort={true}
-        pagination={{
-          limit: 6,
-        }}
-      />
+      <div>
+        <Grid
+          server={{
+            url: "http://localhost:3000/productos",
+            handle: async (res) => {
+              setLoading(false);
+              if (res.status === 404) return await { data: [] };
+              if (res.ok) return await res.json();
+
+              throw new Error("oh no :(");
+            },
+            then: (data) =>
+              data.map((producto) => [
+                producto.nombre,
+                producto.descripcion,
+                parseFloat(producto.precio).toLocaleString("es-MX", {
+                  style: "currency",
+                  currency: "MXN",
+                }),
+                producto.stock,
+                _(
+                  <ActionsTable
+                    setModal={setModal}
+                    handleDelete={handleDelete}
+                    producto={producto}
+                    setSelectedProduct={setSelectedProduct}
+                  />
+                ),
+              ]),
+          }}
+          columns={["Nombre", "Descripción", "Precio", "Stock", "Acciones"]}
+          search={true}
+          sort={true}
+          pagination={{
+            limit: 6,
+          }}
+        />
+      </div>
     </div>
   );
 }
