f2dfb7ca4634b4e7ec82c0fd1c109a5fecb4ceea
EverViera12 <evertigre10@gmail.com>
2024-10-03 14:08:45 -0600

    form styles

--- components/Forms/InstallationForm.jsx
+++ components/Forms/InstallationForm.jsx
@@ -10,90 +10,112 @@ export default function InstallationForm({
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10"
       id="modal_animation"
     >
-      <div className="bg-white shadow-lg rounded-lg p-8 space-y-6 max-w-lg w-full">
+      <div className="bg-white shadow-lg rounded-lg p-8 space-y-6 max-w-2xl w-full">
         <form onSubmit={handleCreateOrUpdate} className="space-y-6 text-black">
-          {/* Select Cliente */}
-          <div>
-            <label
-              className="block text-sm font-medium text-gray-600"
-              htmlFor="cliente"
-            >
-              Cliente
-            </label>
-            <select
-              name="cliente"
-              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md"
-              defaultValue={selectedInstallation?.cliente_id || ""}
-              required
-            >
-              <option value="">Seleccione un cliente</option>
-              {clients.map((client) => (
-                <option key={client.id} value={client.id}>
-                  {client.nombre}
-                </option>
-              ))}
-            </select>
+          {/* fila 1 */}
+          <div className="grid gap-8 grid-cols-3">
+            {/* Select Cliente */}
+            <div>
+              <label
+                className="block text-sm font-medium text-gray-600"
+                htmlFor="cliente"
+              >
+                Cliente
+              </label>
+              <select
+                name="cliente"
+                className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md"
+                defaultValue={selectedInstallation?.cliente_id || ""}
+                required
+              >
+                <option value="">Seleccione un cliente</option>
+                {clients.map((client) => (
+                  <option key={client.id} value={client.id}>
+                    {client.nombre}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            {/* Select Producto */}
+            <div>
+              <label
+                className="block text-sm font-medium text-gray-600"
+                htmlFor="producto"
+              >
+                Producto
+              </label>
+              <select
+                name="producto"
+                className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md"
+                defaultValue={selectedInstallation?.producto_id || ""}
+                required
+              >
+                <option value="">Seleccione un producto</option>
+                {products.map((product) => (
+                  <option key={product.id} value={product.id}>
+                    {product.nombre}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            {/* Select Estado */}
+            <div>
+              <label
+                className="block text-sm font-medium text-gray-600"
+                htmlFor="estado"
+              >
+                Estado
+              </label>
+              <select
+                name="estado"
+                className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md"
+                defaultValue={selectedInstallation?.estado || ""}
+                required
+              >
+                <option value="">Seleccione un estado</option>
+                <option value="pendiente">Pendiente</option>
+                <option value="en_proceso">En proceso</option>
+                <option value="terminado">Terminado</option>
+              </select>
+            </div>
           </div>
 
-          {/* Select Producto */}
-          <div>
-            <label
-              className="block text-sm font-medium text-gray-600"
-              htmlFor="producto"
-            >
-              Producto
-            </label>
-            <select
-              name="producto"
-              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md"
-              defaultValue={selectedInstallation?.producto_id || ""}
-              required
-            >
-              <option value="">Seleccione un producto</option>
-              {products.map((product) => (
-                <option key={product.id} value={product.id}>
-                  {product.nombre}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          {/* Select Estado */}
-          <div>
-            <label
-              className="block text-sm font-medium text-gray-600"
-              htmlFor="estado"
-            >
-              Estado
-            </label>
-            <select
-              name="estado"
-              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md"
-              defaultValue={selectedInstallation?.estado || ""}
-              required
-            >
-              <option value="">Seleccione un estado</option>
-              <option value="pendiente">Pendiente</option>
-              <option value="en_proceso">En proceso</option>
-              <option value="terminado">Terminado</option>
-            </select>
-          </div>
-
+          {/* fila 2 */}
+          <div className="grid gap-8 grid-cols-2">
           {/* Otros campos (fecha, descripción, costo) */}
-          <div>
-            <label
-              className="block text-sm font-medium text-gray-600"
-              htmlFor="fecha_instalacion"
-            >
-              Fecha de Instalación
-            </label>
-            <input
-              type="date"
-              name="fecha_instalacion"
-              defaultValue={selectedInstallation?.fecha_instalacion || ""}
-              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md"
-              required
-            />
+            <div>
+              <label
+                className="block text-sm font-medium text-gray-600"
+                htmlFor="fecha_instalacion"
+              >
+                Fecha de Instalación
+              </label>
+              <input
+                type="date"
+                name="fecha_instalacion"
+                defaultValue={selectedInstallation?.fecha_instalacion || ""}
+                className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md"
+                required
+              />
+            </div>
+
+            <div>
+              <label
+                className="block text-sm font-medium text-gray-600"
+                htmlFor="costo_instalacion"
+              >
+                Costo de Instalación
+              </label>
+              <input
+                type="text"
+                name="costo_instalacion"
+                defaultValue={selectedInstallation?.costo_instalacion || ""}
+                className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md"
+                required
+              />
+            </div>
           </div>
 
           <div>
@@ -107,22 +129,7 @@ export default function InstallationForm({
               name="descripcion"
               defaultValue={selectedInstallation?.descripcion || ""}
               className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-
-          <div>
-            <label
-              className="block text-sm font-medium text-gray-600"
-              htmlFor="costo_instalacion"
-            >
-              Costo de Instalación
-            </label>
-            <input
-              type="text"
-              name="costo_instalacion"
-              defaultValue={selectedInstallation?.costo_instalacion || ""}
-              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md"
+              rows={10}
               required
             />
           </div>
